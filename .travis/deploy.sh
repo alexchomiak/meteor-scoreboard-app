c636dd1a58d41e75b17a3e84b8598e9a7bff947c
alexchomiak <alex.chomiak@gmail.com>
2019-02-14 00:33:39 -0600

    ?

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -10,7 +10,7 @@ if [ $TRAVIS_BRANCH == "master" ] ; then
     eval "$(ssh-agent -s)"
     ssh-add ~/.ssh/travis_rsa
 	git config --global push.default matching
-    git remote add deploy "travis@ssh-proxy.chom.es:/var/www/scoreboardapp/"
+    git remote add deploy "travis@ssh-proxy.chom.es:/var/www/scoreboardapp"
 	git push -f deploy master 
 else   	
 	echo "oof"
