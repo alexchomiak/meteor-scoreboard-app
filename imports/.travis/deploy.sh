f58e33aa9d9433a02d99b54a22ae8d4e85de2631
alexchomiak <alex.chomiak@gmail.com>
2019-02-13 23:44:04 -0600

    testing travis

---
+++ imports/.travis/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# print outputs and exit on first failure
+set -xe
+
+if [ $TRAVIS_BRANCH == "master" ] ; then
+	echo "deploying"
+	echo $(pwd)
+    # setup ssh agent, git config and remote
+    eval "$(ssh-agent -s)"
+    ssh-add ~/.ssh/travis_rsa
+	git config --global push.default matching
+    git remote add deploy "travis@ssh-proxy.chom.es:/var/www/scoreboardapp/"
+	git push -f deploy master 
+else   	
+	echo "oof"
+fi
